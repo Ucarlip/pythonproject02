d465b018158d2106d0d3fedd6b865096040014e8
Ucarlip <modkak98@gmail.com>
2023-08-25 10:24:58 +0700

    code v.2 : วิธีเว้นวัค

--- test01.py
+++ test01.py
@@ -1,1 +1,11 @@
-print('hello')
+print('hello01')
+print("hi....")
+print(23456)
+print(2555.256)
+print(True)
+print(False)
+print(456 + 566 ) #expression นิพจน์
+
+
+#escape sequence คือ รหัส \ -> \n, \t, \", \', \\, \b, .... เวลานำมาใช้ต้องเขียนอยู่ใน string
+print('AAA\nBBB\t\\tCCC\nDEF\b\"XYZ\"')
---
+++ test02.py
@@ -0,0 +1,11 @@
+#1. ใช้ ,
+print("hello...",456,'hi...',True,10+20,"hey....")
+#2. ใช้ + มีข้อเเม้ว่าทุกตัวที่เอามาต่อกันต้องเป็น String
+print("Hello..."+str(456)+'Hi...'+str(True)+' '+str(20+10)+"Hey....")
+#3. ใช้เมธอด Format
+print('Hello{}Hi...{} {} Hay...'.format(456,True,10+20))
+print('Hello{0}Hi...{1} {2} Hay...'.format(456,True,10+20)) #index number ตำเเหน่งข้อมูลในปีกกา
+#4. ใช้ f-string
+print(f"hello...{465} Hi... {True} {120+34} Hey...")
+#5. ใข้ modular operator (%)-> %d, %f, %c, %s,....
+print('Hello... %d Hi... %s %d Hay...' %(456,True,10+20) )
